2fa7badc94cc7d26fa73054dd31deecc679e61c4
huang4fstudio <huang.4fstudio@gmail.com>
2016-02-24 11:01:13 +0000

    Improved login script

--- resources/modules/check_login.php
+++ resources/modules/check_login.php
@@ -1,18 +1,4 @@
 <?php
-<<<<<<< HEAD
-  session_start();
-  include_once('database_connection.php');
-
-  db_connect();
-  if(!isset($_SESSION['name'])){
-     echo 'alert("No user logged in")';
-     header("location: login.php");
-     die;
-  }
-  else {
-    echo 'alert("User logged in")';
-    header("location: profile.php");
-=======
   function check_login($require_login) {
     session_start();
     if($require_login) {
@@ -26,7 +12,6 @@
             die;
         }
     }
->>>>>>> dc07be2d6789bd6a0f900c1e366c170a25632942
   }
   
 ?>
