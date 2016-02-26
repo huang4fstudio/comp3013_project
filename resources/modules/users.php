9788e7457b1cfea31f673f480968831030be8f78
huang4fstudio <huang.4fstudio@gmail.com>
2016-02-26 10:42:18 +0000

    Login functionality

--- resources/modules/users.php
+++ resources/modules/users.php
@@ -4,13 +4,16 @@
        return db_fetch_array("SELECT * FROM users WHERE name='$username' AND password='$password'");
     }
 
+    function find_user_email($email) {
+        return db_fetch_array("SELECT * FROM User_id WHERE email='$email'");
+    }
+
     function find_user_username_id($username, $id) {
         return db_fetch_array("SELECT * FROM users WHERE username='$username' AND id='$id'");
     }
 
     function find_user_username_id_email($username, $id, $email) {
         return db_fetch_array("SELECT * FROM users WHERE username='$username' AND id='$id' AND email='$email'");
-       
     }
 
     function find_user_id($id) {
