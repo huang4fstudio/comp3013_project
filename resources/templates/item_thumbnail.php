fd8f6996a11746159ba70f4fbe1f006039e2e657
Meckila <mbritt@wellesley.edu>
2016-02-29 10:56:11 +0000

    Thumbnail Template

---
+++ resources/templates/item_thumbnail.php
@@ -0,0 +1,8 @@
+<div class="item-thumbnail">
+  <img src="img/placeholder.png" class="thumbnail">
+  <span>
+      Lorem ipsum dolor sit amet
+      <br>
+      <strong name="itemPrice">£0.00</strong>
+  </span>
+</div>
