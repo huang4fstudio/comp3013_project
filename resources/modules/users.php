343ec4346af294919d93ebf0d74eeba66c3770c9
huang4fstudio <huang.4fstudio@gmail.com>
2016-02-29 10:47:43 +0000

    Users

--- resources/modules/users.php
+++ resources/modules/users.php
@@ -1,7 +1,7 @@
 <?php
-    require_once("./database_connection.php");
+    require_once("database_connection.php");
     function find_user_username_password($username, $password) {
-       return db_fetch_array("SELECT * FROM users WHERE name='$username' AND password='$password'");
+       return db_fetch_array("SELECT * FROM User WHERE name='$username' AND password='$password'");
     }
 
     function find_user_email($email) {
@@ -9,14 +9,14 @@
     }
 
     function find_user_username_id($username, $id) {
-        return db_fetch_array("SELECT * FROM users WHERE username='$username' AND id='$id'");
+        return db_fetch_array("SELECT * FROM User WHERE username='$username' AND id='$id'");
     }
 
     function find_user_username_id_email($username, $id, $email) {
-        return db_fetch_array("SELECT * FROM users WHERE username='$username' AND id='$id' AND email='$email'");
+        return db_fetch_array("SELECT * FROM User WHERE username='$username' AND id='$id' AND email='$email'");
     }
 
     function find_user_id($id) {
-        return db_fetch_array("SELECT * FROM Users WHERE id='$id'");
+        return db_fetch_array("SELECT * FROM User WHERE id='$id'");
     }
 ?>
