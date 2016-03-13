2389f8a6c2d6a7a3b436cddca826cca1663d4314
Meckila <mbritt@wellesley.edu>
2016-03-13 22:08:01 +0000

    Sorting function

--- public_html/js/ebid.js
+++ public_html/js/ebid.js
@@ -1,9 +1,45 @@
+
+$(document).ready(function () {
+  $('#sortQuery').change(function () {
+     var valueSelected = this.value;
+
+     if(valueSelected=="priceHiLo"){
+       alert("Sort high low");
+       sortPriceLowHigh($('#searchResults'), "div", "span.strong.itemPrice");
+     }
+     else if(valueSelected="priceLoHi"){
+       alert("Sort low high");
+       sortPriceLowHigh($('#searchResults'), "div", "span.strong.itemPrice");
+     }
+  });
+});
+
+function sortPriceLowHigh(parent, childSelector, keySelector) {
+    var items = parent.children(childSelector).sort(function(a, b) {
+        var vA = $(keySelector, a).text();
+        var vB = $(keySelector, b).text();
+        return (vA < vB) ? -1 : (vA > vB) ? 1 : 0;
+    });
+    parent.empty();
+    console.log(items);
+    parent.append(items);
+}
+
+function sortPriceHighLow(parent, childSelector, keySelector) {
+    var items = parent.children(childSelector).sort(function(a, b) {
+        var vA = $(keySelector, a).text();
+        var vB = $(keySelector, b).text();
+        return (vA > vB) ? -1 : (vA < vB) ? 1 : 0;
+    });
+    parent.empty();
+    console.log(items);
+    parent.append(items);
+}
+
 window.onload = function() {
   $('.input-group.date').datepicker({
     todayHighlight: true
   });
-
-
 };
 
 function countChar(val) {
